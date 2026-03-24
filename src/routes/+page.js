cda44e250e86c87dc3d26b0d5b5c62e54a5c57d9
jack-walk <jacktmwalker@gmail.com>
2026-03-24 09:54:26 -0400

    classwork

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -1,10 +1,13 @@
 // Page settings
+import permits from '$lib/data/permits.json';
+
 // These values are passed to the layout to control what appears on the page.
 export function load() {
   return {
     // Set to false to hide the NYCity News Service header
-    showHeader: true,
+    showHeader: true, 
     // Set to false to hide the site footer
     showFooter: true,
+    permits,
   };
 }
